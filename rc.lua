91a669cee48101ab14d78fd7555fa967331ed255
Andreas Madsack <andreas@madflex.de>
2014-07-03 16:15:57 +0200

    add run_once method and some things you want to start

--- rc.lua
+++ rc.lua
@@ -426,3 +426,27 @@ end)
 client.add_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.add_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+
+-- autostart:
+function run_once(prg,arg_string,pname,screen)
+    if not prg then
+        do return nil end
+    end
+
+    if not pname then
+       pname = prg
+    end
+
+    if not arg_string then 
+        awful.util.spawn_with_shell("pgrep -f -u $USER -x '" .. pname .. "' || (" .. prg .. ")",screen)
+    else
+        awful.util.spawn_with_shell("pgrep -f -u $USER -x '" .. pname .. "' || (" .. prg .. " " .. arg_string .. ")",screen)
+    end
+end
+
+run_once("xscreensaver","-no-splash")
+-- run_once("nm-applet","")
+run_once("xfsettingsd","")
+run_once("xfce4-power-manager","")
+run_once("setxkbmap -model pc105 -layout de -variant nodeadkeys")
