d3ddda3b9cd605e6e56349519e21d1025b5eaae0
Andreas Madsack <andreas@madflex.de>
2015-07-20 14:07:52 +0200

    remove max layouts from layoutlist

--- rc.lua
+++ rc.lua
@@ -87,9 +87,9 @@ layouts =
     awful.layout.suit.fair.horizontal,
     awful.layout.suit.spiral,
     awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max,
-    awful.layout.suit.max.fullscreen,
-    awful.layout.suit.magnifier
+--    awful.layout.suit.max,
+--    awful.layout.suit.max.fullscreen,
+--    awful.layout.suit.magnifier
     awful.layout.suit.floating,
 }
 -- }}}
